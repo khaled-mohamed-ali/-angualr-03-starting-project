003e27473442d947cab441c26be892a531c00828
khaled mohammed <dev.khaled.mohamed@gmail.com>
2024-10-15 21:43:14 +0300

    use attr directive

--- src/app/safe-link.directive.ts
+++ src/app/safe-link.directive.ts
@@ -1,10 +1,24 @@
-import { Directive } from '@angular/core';
+import { Directive, input } from '@angular/core';
 
 @Directive({
   selector: '[appSafeLink]',
-  standalone: true
+  standalone: true,
+  host: {'(click)': 'clickToDo($event)'}
 })
 export class SafeLinkDirective {
+  queryParam = input('myApp')
+  
+  constructor() {
+  }
 
+  clickToDo(event: MouseEvent) {
 
+    const wantToLeave = window.confirm('to you want to leave');
+    if(wantToLeave) {
+      const address = (event.target as HTMLAnchorElement).href;
+        (event.target as HTMLAnchorElement).href = address + '?from=' + this.queryParam();
+        return
+    }
+    event.preventDefault()
+  }
 }
